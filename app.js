302130481fdd1d50888ebebf8ded8a1fab8aa83e
Teinaki <61263141+Teinaki@users.noreply.github.com>
2020-11-04 09:44:39 +1300

    Added heroku

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('assets'));
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/info', (req, res) => {
+    res.render('info')
+});
+
+
+
+app.listen(3000);
