090865284b07e73d55f84cd8a108a9c45c9afe41
Teinaki McFelin <MCFETK1@student.op.ac.nz>
2020-11-05 21:17:16 +1300

    Added new page with Updated CSS

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get('/info', (req, res) => {
     res.render('info')
 });
 
+app.get('/sales', (req, res) => {
+    res.render('sales')
+});
+
+app.get('/img', (req, res) => {
+    res.render('img')
+});
+
 
 
 app.listen(process.env.PORT || 3000)
